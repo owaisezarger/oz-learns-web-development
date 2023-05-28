2d38c7aaa317a4323002702cd62ac86df9ef1a5f
Owaise Zarger <owaisezarger@gmail.com>
2023-05-28 16:01:57 +0530

    initial commit - folder structure

---
+++ js101/introduction-to-js-variables-and-data-types/problem4.js
@@ -0,0 +1,16 @@
+/*
+Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+Print the report card of the student (You can make it look nice by using some keyboard symbols )
+Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+*/
+let name = "Owaise";
+let school = "Masai School";
+let grade = "Revision";
+let section = "MERN";
+let rollno = "fw20_0075";
+let DSA = 5;
+let JS = 6;
+let MAC = 7;
+
+console.log("| Name | School | Grade | Section | Roll No | DSA | JS | MAC |");
+console.log("|"+ name + "|" + school + "|" + grade + "|" + section +"|"+ rollno + "|" + DSA + "|"+ JS + "|" + MAC + "|");
