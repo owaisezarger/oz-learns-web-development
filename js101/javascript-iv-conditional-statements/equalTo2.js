aab4020e350671bd2a1ec19410bcdf2fc26c9e6d
Owaise Zarger <owaisezarger@gmail.com>
2023-05-28 19:53:38 +0530

    initial commit - conditional statemeants

---
+++ js101/javascript-iv-conditional-statements/equalTo2.js
@@ -0,0 +1,16 @@
+/*
+You are given a number, stored in the variable with the name N
+Print Yes , if the number is equal to 2, else print No.
+*/
+
+function equalTo2(N) {
+    // Write code here
+    if(N === 2){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
+}
+
+equalTo2(2);
+equalTo2(3);
