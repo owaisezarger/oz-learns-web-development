046c38574238408bd476a8bcec7f1d02f3786972
Owaise Zarger <owaisezarger@gmail.com>
2023-05-30 02:45:15 +0530

    todo app

---
+++ rct101/sprint-1/todo/src/Components/Todos.jsx
@@ -0,0 +1,30 @@
+import React,{useState} from "react";
+
+const Todos = () =>{
+    const [text, setText] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = () =>{
+        const newTodo = {
+            text: text,
+            id: Date.now(),
+        }
+        setTodos([...todos, newTodo]);
+        setText("");
+    }
+
+    return(
+        <div>
+            <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+            <button onClick={addTodo}>Add</button>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>{todo.text}</li>
+                ))}
+            </ul>
+        </div>
+
+    )
+}
+
+export default Todos;
